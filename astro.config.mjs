b7857674cbf1cc690fc4177bbb0389373785157b
lucascontreras9617-sys <lucascontreras9617@gmail.com>
2026-06-11 17:17:41 -0300

    automatizacion de sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://tuc-humano.com',
   output: 'hybrid',
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
     remotePatterns: [{ protocol: 'https' }],
